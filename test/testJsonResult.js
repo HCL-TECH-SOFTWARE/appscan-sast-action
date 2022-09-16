a7bedaa6daad20980c3b28c2ac83444bd3ba9a26
mattmurp <matthew.murphy@hcl.com>
2022-09-16 09:06:11 -0400

    Copyright (#2)
    * add copyrights
    
    * remove test folder

--- test/testJsonResult.js
+++
@@ -1,9 +0,0 @@
-let jsonString = '[{"Severity": "Informational","Count": 0},{"Severity": "Low","Count": 0 },{"Severity": "Medium","Count": 1},{"Severity": "High", "Count": 0}, { "Severity": "Critical", "Count": 0 }]'
-let json = JSON.parse(jsonString);
-
-let output = '';
-json.forEach(element => {
-    output += element.Severity + ' = ' + element.Count + '\n';
-});
-
-console.log(output);
