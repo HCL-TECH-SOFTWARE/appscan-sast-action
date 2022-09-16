6a4223aa7a894526cfc2b667edbd5d50ce20562b
Matthew Murphy <matthew.murphy@hcl.com>
2022-09-16 09:11:29 -0400

    update  copyrights

--- src/resultProcessor.js
+++ src/resultProcessor.js
@@ -1,4 +1,5 @@
-/*Copyright 2022 HCL Technologies Ltd.
+/*
+Copyright 2022 HCL America, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
