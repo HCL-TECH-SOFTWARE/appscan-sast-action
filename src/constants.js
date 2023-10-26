b20baa15c73dd044b4771592397e0a1f7b767413
Matthew <matthew.murphy@hcl.com>
2023-10-26 12:29:17 -0400

    clean up constants

--- src/constants.js
+++ src/constants.js
@@ -19,73 +19,51 @@ const _CURRENT_VERSION = CURRENT_VERSION;
 export { _CURRENT_VERSION as CURRENT_VERSION };
 
 //Service url and endpoints:
-const SERVICE_URL = 'https://cloud.appscan.com';
-const _SERVICE_URL = SERVICE_URL;
+const _SERVICE_URL = 'https://cloud.appscan.com';
 export { _SERVICE_URL as SERVICE_URL };
-const SACLIENT_PATH = '/api/SCX/StaticAnalyzer/SAClientUtil?os=';
-const _SACLIENT_PATH = SACLIENT_PATH;
+const _SACLIENT_PATH = '/api/SCX/StaticAnalyzer/SAClientUtil?os=';
 export { _SACLIENT_PATH as SACLIENT_PATH };
-const API_LOGIN = '/api/V2/Account/ApiKeyLogin';
-const _API_LOGIN = API_LOGIN;
+const _API_LOGIN = '/api/V2/Account/ApiKeyLogin';
 export { _API_LOGIN as API_LOGIN };
-const API_SCAN_COUNT_BY_SEVERITY = '/api/v2/Issues/CountBySeverity/scan/';
-const _API_SCAN_COUNT_BY_SEVERITY = API_SCAN_COUNT_BY_SEVERITY;
+const _API_SCAN_COUNT_BY_SEVERITY = '/api/v2/Issues/CountBySeverity/scan/';
 export { _API_SCAN_COUNT_BY_SEVERITY as API_SCAN_COUNT_BY_SEVERITY };
-const CLIENT_TYPE = 'github-sast';
-const _CLIENT_TYPE = CLIENT_TYPE;
+const _CLIENT_TYPE = 'github-sast';
 export { _CLIENT_TYPE as CLIENT_TYPE };
 
 //User messages:
-const DOWNLOADING_CLIENT = 'Downloading the SAClientUtil...';
-const _DOWNLOADING_CLIENT = DOWNLOADING_CLIENT;
+const _DOWNLOADING_CLIENT = 'Downloading the SAClientUtil...';
 export { _DOWNLOADING_CLIENT as DOWNLOADING_CLIENT };
-const GENERATING_IRX = 'Generating irx file...'
-const _GENERATING_IRX = GENERATING_IRX;
+const _GENERATING_IRX = 'Generating irx file...';
 export { _GENERATING_IRX as GENERATING_IRX };
-const AUTHENTICATE_ASOC = 'Authenticating with the ASoC service...';
-const _AUTHENTICATE_ASOC = AUTHENTICATE_ASOC;
+const _AUTHENTICATE_ASOC = 'Authenticating with the ASoC service...';
 export { _AUTHENTICATE_ASOC as AUTHENTICATE_ASOC };
-const SUBMITTING_IRX = 'Submitting the irx for analysis...';
-const _SUBMITTING_IRX = SUBMITTING_IRX;
+const _SUBMITTING_IRX = 'Submitting the irx for analysis...';
 export { _SUBMITTING_IRX as SUBMITTING_IRX };
-const IRX_SUBMIT_SUCCESS = 'Successfully submitted the irx to the ASoC service.';
-const _IRX_SUBMIT_SUCCESS = IRX_SUBMIT_SUCCESS;
+const _IRX_SUBMIT_SUCCESS = 'Successfully submitted the irx to the ASoC service.';
 export { _IRX_SUBMIT_SUCCESS as IRX_SUBMIT_SUCCESS };
-const WAIT_FOR_ANALYSIS = 'Waiting for analysis to complete...';
-const _WAIT_FOR_ANALYSIS = WAIT_FOR_ANALYSIS;
+const _WAIT_FOR_ANALYSIS = 'Waiting for analysis to complete...';
 export { _WAIT_FOR_ANALYSIS as WAIT_FOR_ANALYSIS };
-const GETTING_RESULTS = 'Getting results...';
-const _GETTING_RESULTS = GETTING_RESULTS;
+const _GETTING_RESULTS = 'Getting results...';
 export { _GETTING_RESULTS as GETTING_RESULTS };
-const ANALYSIS_SUCCESS = 'Analysis complete.';
-const _ANALYSIS_SUCCESS = ANALYSIS_SUCCESS;
+const _ANALYSIS_SUCCESS = 'Analysis complete.';
 export { _ANALYSIS_SUCCESS as ANALYSIS_SUCCESS };
-const ANALYSIS_TIMEOUT = 'Timed out waiting for analysis to complete. Review the scan in ASoC to see the results.'
-const _ANALYSIS_TIMEOUT = ANALYSIS_TIMEOUT;
+const _ANALYSIS_TIMEOUT = 'Timed out waiting for analysis to complete. Review the scan in ASoC to see the results.'
 export { _ANALYSIS_TIMEOUT as ANALYSIS_TIMEOUT };
-const TOTAL_ISSUES = 'Total issues: ';
-const _TOTAL_ISSUES = TOTAL_ISSUES;
+const _TOTAL_ISSUES = 'Total issues: ';
 export { _TOTAL_ISSUES as TOTAL_ISSUES };
-const ISSUES_COLON = ' issues: ';
-const _ISSUES_COLON = ISSUES_COLON;
+const _ISSUES_COLON = ' issues: ';
 export { _ISSUES_COLON as ISSUES_COLON };
 
 //Error messages:
-const ERROR_DOWNLOADING_CLIENT = 'An error occurred downloading the SAClientUtil. Status code ';
-const _ERROR_DOWNLOADING_CLIENT = ERROR_DOWNLOADING_CLIENT;
+const _ERROR_DOWNLOADING_CLIENT = 'An error occurred downloading the SAClientUtil. Status code ';
 export { _ERROR_DOWNLOADING_CLIENT as ERROR_DOWNLOADING_CLIENT };
-const ERROR_FILE_DOES_NOT_EXIST = 'An error occurred extracting the SAClientUtil. The file does not exist: '
-const _ERROR_FILE_DOES_NOT_EXIST = ERROR_FILE_DOES_NOT_EXIST;
+const _ERROR_FILE_DOES_NOT_EXIST = 'An error occurred extracting the SAClientUtil. The file does not exist: '
 export { _ERROR_FILE_DOES_NOT_EXIST as ERROR_FILE_DOES_NOT_EXIST };
-const ERROR_INVALID_APP_ID = 'Invalid application ID.'
-const _ERROR_INVALID_APP_ID = ERROR_INVALID_APP_ID;
+const _ERROR_INVALID_APP_ID = 'Invalid application ID.'
 export { _ERROR_INVALID_APP_ID as ERROR_INVALID_APP_ID };
-const ERROR_ANALYSIS_FAILED = 'Analysis failed. Review the scan in ASoC for additional details.'
-const _ERROR_ANALYSIS_FAILED = ERROR_ANALYSIS_FAILED;
+const _ERROR_ANALYSIS_FAILED = 'Analysis failed. Review the scan in ASoC for additional details.'
 export { _ERROR_ANALYSIS_FAILED as ERROR_ANALYSIS_FAILED };
-const ERROR_NONCOMPLIANT_ISSUES = 'Failed. Non-compliant issues were found in the scan.';
-const _ERROR_NONCOMPLIANT_ISSUES = ERROR_NONCOMPLIANT_ISSUES;
+const _ERROR_NONCOMPLIANT_ISSUES = 'Failed. Non-compliant issues were found in the scan.';
 export { _ERROR_NONCOMPLIANT_ISSUES as ERROR_NONCOMPLIANT_ISSUES };
-const ERROR_BAD_SCAN_ID = 'An error occurred submitting the irx for analysis.';
-const _ERROR_BAD_SCAN_ID = ERROR_BAD_SCAN_ID;
+const _ERROR_BAD_SCAN_ID = 'An error occurred submitting the irx for analysis.';
 export { _ERROR_BAD_SCAN_ID as ERROR_BAD_SCAN_ID };
