c0ecb277f7bceb96300b9acf03cc44389cd8d921
Matthew <matthew.murphy@hcl.com>
2026-04-27 13:39:20 -0400

    update how prepare command is run

--- src/client.js
+++ src/client.js
@@ -14,138 +14,52 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import eol from 'eol';
-import shell from 'shelljs';
-import * as constants  from './constants.js';
+import child_process from {child_process}
 import saclientutil from './saclientutil.js';
 import utils from './utils.js';
 import settings from './settings.js';
 
-let start = null;
-const timeout_minutes = process.env.INPUT_ANALYSIS_TIMEOUT_MINUTES ? process.env.INPUT_ANALYSIS_TIMEOUT_MINUTES : 30;
-
-shell.cd(process.env.GITHUB_WORKSPACE);
 process.env.APPSCAN_IRGEN_CLIENT = 'GitHubSast';
 process.env.IRGEN_CLIENT_PLUGIN_VERSION = utils.getVersion();
 
 function generateIrx() {
-    let args = '-sco '; //Default to running source code only scans.
+    let args = [];
+
+    if(!isArgumentEnabled(process.env.INPUT_SCAN_BUILD_OUTPUTS)) {
+        args.push('-sco');
+    }
 
     if(isArgumentEnabled(process.env.INPUT_STATIC_ANALYSIS_ONLY)) {
-        args += '-sao ';
+        args.push('-sao');
     }
     if(isArgumentEnabled(process.env.INPUT_OPEN_SOURCE_ONLY)) {
-        args += '-oso ';
+        args.push('-oso');
     }
     if(isArgumentEnabled(process.env.INPUT_SECRETS_ONLY)) {
-        args += '-so ';
-        args += '-sao ';
-    }
-	
-    if(isArgumentEnabled(process.env.INPUT_SCAN_BUILD_OUTPUTS)) {
-        args = args.replace('-sco ', '');
+        args.push('-so');
+        args.push('-sao');
     }
 
     return executeCommand(`prepare ${args}`);
 }
 
-function login() {
-    let key = utils.sanitizeString(process.env.INPUT_ASOC_KEY);
-    let secret = utils.sanitizeString(process.env.INPUT_ASOC_SECRET);
-    let options = "api_login -u " + key + " -P " + secret;
-    if (process.env.INPUT_SERVICE_URL) {
-	    let service_url = utils.sanitizeString(process.env.INPUT_SERVICE_URL);
-	    options += " -service_url " + service_url;
-    }
-    
-    return executeCommand(`${options}`);
-}
-
-function runAnalysis() {
-    return new Promise((resolve, reject) => {
-        let args = '';
-
-        let scanNameOption = utils.sanitizeString(process.env.INPUT_SCAN_NAME);
-        if(scanNameOption) {
-            args += '-n \"' + scanNameOption + '\"';
-        }
-
-        if(isArgumentEnabled(process.env.INPUT_PERSONAL_SCAN)) {
-            args += ' -ps';
-        }
-		
-        let commentOption = utils.sanitizeString(process.env.INPUT_COMMENT);
-        if (commentOption) {
-            args += ' -c \"' + commentOption + '\"';
-        }
-
-        let appId = utils.sanitizeString(process.env.INPUT_APPLICATION_ID);
-        if(!appId) {
-            reject(constants.ERROR_INVALID_APP_ID);
-            return;
-        }
-		
-        executeCommand(`queue_analysis -a ${appId} ${args}`)
-        .then((stdout) => {
-            //Get the scan id from stdout and return it.
-            return getScanId(stdout);
-        })
-        .then((scanId) => {
-            resolve(scanId);
-        })
-        .catch((error) => {
-            reject(error);
-        })
-    });
-}
-
-function checkStatus(scanId) {
-    return executeCommand(`status -i ${scanId}`);
-}
-
-function waitForAnalysis(scanId) {
-    return new Promise((resolve, reject) => {
-        if(!start) {
-            start = Date.now();
-        }
-
-        checkStatus(scanId)
-        .then((stdout) => {
-            if(stdout.trim() === 'Ready') {
-                return resolve(false);
-            }
-            else if(stdout.trim() === 'Failed') {
-                return reject(constants.ERROR_ANALYSIS_FAILED);
-            }
-            else if(analysisTimedOut()) {
-                return resolve(true);
-            }
-            else {
-                setTimeout(() => {
-                    return resolve(waitForAnalysis(scanId));
-                }, 30000)
-            }
-        })
-        .catch((error) => {
-            reject(error);
-        })
-    });
-}
-
 function executeCommand(args) {
     return new Promise((resolve, reject) => {
         if (settings.shouldDisableSSL()) {
-            args += " -acceptssl";
+            args.push('-acceptssl');
         }
 
         saclientutil.getScript()
         .then((script) => {
-            let result = shell.exec(`${script} ${args}`);
-            if(result.code === 0) {
-                resolve(result.stdout);
+            let result = child_process.spawnSync(script, args, { 
+                encoding: 'utf-8',
+                cwd: process.env.GITHUB_WORKSPACE
+            });
+            if(result.error) {
+                reject(result.stderr);
             }
             else {
-                reject(result.stderr);
+                resolve(result.stdout);
             }
         })
         .catch((error) => {
@@ -158,32 +72,4 @@ function isArgumentEnabled(arg) {
     return arg && arg === 'true';
 }
 
-function analysisTimedOut() {
-    let seconds = (Date.now() - start) / 1000;
-    let minutes = seconds / 60;
-    return minutes > timeout_minutes;
-}
-
-function getScanId(output) {
-    return new Promise((resolve) => {
-        let lines = eol.split(output.trim());
-        let scanId = lines[lines.length - 1];
-        //Make sure we have a valid scan id.
-        let regex = /[0-9a-fA-f]{8}-[0-9a-fA-f]{4}-[0-9a-fA-f]{4}-[0-9a-fA-f]{4}-[0-9a-fA-f]{12}/;
-        let matches = scanId.match(regex);
-        
-        if(!matches) {
-            //If we didn't get a match, check the previous line
-            scanId = lines[lines.length - 2];
-            matches = scanId.match(regex);
-            if(!matches) {
-                //If still no match, log it.
-                return resolve(constants.NO_SCAN_ID);
-            }
-        }
-
-        resolve(matches[0]);
-    })
-}
-
-export default { generateIrx, login, runAnalysis, waitForAnalysis }
+export default { generateIrx }
